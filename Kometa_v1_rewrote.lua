b49716af1e5d8173590aaad3e7341735726bfab5
noobball111 <59442213+noobball111@users.noreply.github.com>
2023-01-23 17:15:12 +0700

    Update Kometa_v1_rewrote.lua

--- Kometa_v1_rewrote.lua
+++ Kometa_v1_rewrote.lua
@@ -1,3 +1,4 @@
+time1 = time()
 local api = loadstring(game:HttpGet("https://raw.githubusercontent.com/noobball111/Kometa_rewrite_by_rqp/main/Kometa_v1_api_rewrote.lua"))()
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/kometa-anon/kometa/main/ui/finity.lua"))()
 local bssapi = loadstring(game:HttpGet("https://raw.githubusercontent.com/kometa-anon/kometa/main/api/bssapi.lua"))()
