4be66f48e61372bc1fd56e830018bdc0712dcf6e
noobball111 <59442213+noobball111@users.noreply.github.com>
2023-01-27 11:35:02 +0700

    Update Kometa_v1_rewrote.lua

--- Kometa_v1_rewrote.lua
+++ Kometa_v1_rewrote.lua
@@ -774,7 +774,13 @@ function killmobs()
 				end
 				-- api.humanoidrootpart().CFrame = monsterpart.CFrame
 				api.tween(3, monsterpart.CFrame + Vector3.new(0, 30, 0))
-				repeat avoidmob() task.wait(1.2) api.tween(3, monsterpart.CFrame + Vector3.new(0, 30, 0)) until v:FindFirstChild("TimerLabel", true).Visible or api.humanoid().Health == 0
+				repeat 
+					avoidmob() 
+					task.wait(1.2) 
+					if api.distance(api.humanoidrootpart.Position, monsterpart.CFrame) ~= 15 then
+						api.tween(3, monsterpart.CFrame)
+					end
+				until v:FindFirstChild("TimerLabel", true).Visible or api.humanoid().Health == 0
 				-- repeat api.humanoidrootpart().CFrame = monsterpart.CFrame avoidmob() task.wait(1) until v:FindFirstChild("TimerLabel", true).Visible or api.humanoid().Health == 0
 				for i = 1, 4 do gettoken(monsterpart.Position) end
 			end
